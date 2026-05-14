340d4ded67751a01c6bd151f8c9cb616a89b6145
LuizPav <luizpavao35@gmail.com>
2026-05-14 13:01:31 -0300

    [Passo 2] ViewModel

---
+++ app/src/main/java/com/example/weatherapp/ui/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.weatherapp.ui
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+import com.example.weatherapp.model.City
+
+class MainViewModel: ViewModel() {
+    private val _cities = List(20) { i ->
+        City(name = "Cidade $i", weather = "Carregando Clima...")
+    }.toMutableStateList()
+    val cities
+        get() = _cities.toList()
+    fun remove(city: City) {
+        _cities.remove(city)
+    }
+    fun add(name: String) {
+        _cities.add(City(name = name))
+    }
+}
